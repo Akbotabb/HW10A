d55ad3cad1342018d9dc902770ee2d417d148e00
Akbota <omarbek.akbota@gmail.com>
2023-04-24 19:05:20 +0600

    remove comments

--- HW10A/ViewController.swift
+++ HW10A/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  HW10A
-//
-//  Created by Akbota Omarbek on 24.04.2023.
-//
+
 
 import UIKit
 
@@ -12,7 +7,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemRed
-        // Do any additional setup after loading the view.
+
     }
 
 
