b79917034608286e1dbb0398c5a7c9ea8d105c4e
Akbota <omarbek.akbota@gmail.com>
2023-04-24 19:15:18 +0600

    remove comments

--- HW10A/ViewController.swift
+++ HW10A/ViewController.swift
@@ -1,17 +1,11 @@
-//
-//  ViewController.swift
-//  HW10A
-//
-//  Created by Akbota Omarbek on 24.04.2023.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
