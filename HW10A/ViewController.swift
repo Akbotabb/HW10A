7a098d12a9afe51ba5a2676e8078d0eda77b8798
Akbota <omarbek.akbota@gmail.com>
2023-04-24 18:28:59 +0600

    Initial Commit

---
+++ HW10A/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10A
+//
+//  Created by Akbota Omarbek on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
